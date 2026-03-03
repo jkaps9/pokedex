e398dae678a6269cd1f569f886f58c5c0cc018cf
Josh <15219491+jkaps9@users.noreply.github.com>
2026-03-03 16:18:57 -0500

    Update help command to use the State object

--- src/command_help.ts
+++ src/command_help.ts
@@ -1,8 +1,8 @@
-import type { CLICommand } from "./CLICommand.js";
+import type { State } from "./state.js";
 
-export function commandHelp(commands: Record<string, CLICommand>) {
+export function commandHelp(state: State) {
   console.log("Welcome to the Pokedex!\nUsage:\n");
-  Object.values(commands)
+  Object.values(state.commandRegistry)
     .sort((a, b) => b.name.localeCompare(a.name))
     .forEach((command) => {
       console.log(`${command.name}: ${command.description}`);
