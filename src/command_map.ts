98ff85b4b202189cdfd3a68cd3e13d6eb00d51f9
Josh <15219491+jkaps9@users.noreply.github.com>
2026-03-03 17:28:36 -0500

    update map callback to handle the response

--- src/command_map.ts
+++ src/command_map.ts
@@ -1,6 +1,15 @@
 import type { State } from "./state.js";
+import type { Location } from "./pokeapi.js";
 
 export async function commandMap(state: State) {
-  console.log("Fetching locations...");
-  state.pokeAPI.fetchLocations();
+  try {
+    const data = await state.pokeAPI.fetchLocations();
+    data.results.forEach((loc: Location) => {
+      console.log(loc.name);
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error.message);
+    }
+  }
 }
