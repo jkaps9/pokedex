a9c04a4c5cf57b3483ab1927db6e8a4e4a16d60f
Josh <15219491+jkaps9@users.noreply.github.com>
2026-03-03 14:40:31 -0500

    create a registry of commands

--- src/repl.ts
+++ src/repl.ts
@@ -29,3 +29,20 @@ export function commandExit() {
   console.log("Closing the Pokedex... Goodbye!");
   process.exit(0);
 }
+
+export type CLICommand = {
+  name: string;
+  description: string;
+  callback: (commands: Record<string, CLICommand>) => void;
+};
+
+export function getCommands(): Record<string, CLICommand> {
+  return {
+    exit: {
+      name: "exit",
+      description: "Exits the pokedex",
+      callback: commandExit,
+    },
+    // can add more commands here
+  };
+}
