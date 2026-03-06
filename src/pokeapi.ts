dd89d8e11aacd08e92a5451a2c507d981f129d4c
p10pro <15219491+jkaps9@users.noreply.github.com>
2026-03-06 08:54:51 -0500

    add cache object to pokeapi

--- src/pokeapi.ts
+++ src/pokeapi.ts
@@ -1,16 +1,28 @@
+import { Cache } from "./pokecache.js";
+
 export class PokeAPI {
   private static readonly baseURL = "https://pokeapi.co/api/v2";
 
-  constructor() {}
+  private cache: any;
+
+  constructor() {
+    this.cache = new Cache(300000); // 5 min cache (300k ms)
+  }
 
   async fetchLocations(pageURL?: string): Promise<ShallowLocations> {
-    //Promise<ShallowLocations>
-
+    
     const url = pageURL ? pageURL : PokeAPI.baseURL + "/location-area/";
-    const response = await fetch(url);
+
+    const response = this.cache.has(url) ? this.cache.get(url) : await fetch(url);
 
     if (!response.ok) {
       throw new Error(`Response status: ${response.status}`);
+    }
+
+    if(!this.cache.has(url)){
+      this.cache.add(url, response);
+    } else {
+      console.log("using cached response");
     }
 
     return response.json() as Promise<ShallowLocations>;
