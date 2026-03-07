0afef1e51e2ff0b7054a1a7b7c200fbc9257fc41
Josh <15219491+jkaps9@users.noreply.github.com>
2026-03-06 21:14:33 -0500

    fix command explore to display pokemon names

--- src/command_explore.ts
+++ src/command_explore.ts
@@ -2,10 +2,12 @@ import type { State } from "./state.js";
 import type { Location } from "./pokeapi.js";
 
 export async function commandExplore(state: State, locationName: string) {
+  console.log(`Exploring ${locationName}`);
   try {
     const data = await state.pokeAPI.fetchLocation(locationName);
-    data.pokemon_encounters.forEach((loc: Location) => {
-      console.log(loc);
+    console.log("Found Pokemon:");
+    data.pokemon_encounters.forEach((pokemon: any) => {
+      console.log(` - ${pokemon.pokemon.name}`);
     });
   } catch (error) {
     if (error instanceof Error) {
