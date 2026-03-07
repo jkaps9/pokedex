0e1d466e544fb0a34bccce7405b6f78e2088a8d3
Josh <15219491+jkaps9@users.noreply.github.com>
2026-03-06 22:11:46 -0500

    sort help commands A->Z

--- src/command_help.ts
+++ src/command_help.ts
@@ -3,7 +3,7 @@ import type { State } from "./state.js";
 export async function commandHelp(state: State) {
   console.log("Welcome to the Pokedex!\nUsage:\n");
   Object.values(state.commandRegistry)
-    .sort((a, b) => b.name.localeCompare(a.name))
+    .sort((a, b) => a.name.localeCompare(b.name))
     .forEach((command) => {
       console.log(`${command.name}: ${command.description}`);
     });
